4b2cb5d74d8ecd49421766d62153529576fdc773
jcostales10 <costaj28@uw.edu>
2021-05-13 09:57:38 -0700

    add solutions

--- app_server.R
+++ app_server.R
@@ -7,12 +7,18 @@ library(shiny)
 # load data
 immunizations <- read.csv("immunizations.csv")
 
+# purpose: to determine if greater enrollment size affects immunization rates by
+# county 
+
+
 # ------- CLEAN DATA --------- 
 # should have columns County, Percent_complete_for_all_immunizations and 
 # K_12_enrollment
 # na values dropped
 
-
+immunizations_cleaned <- immunizations %>%
+  select(County, Percent_complete_for_all_immunizations, K_12_enrollment) %>%
+  drop_na()
 
 
 # ------- INTERACTIVE VISUALIZAION PLOT ------- 
@@ -24,11 +30,21 @@ server <- function(input, output) {
   # insert code for chart here
   
 
-    # filters the dataset from the widgets 
+  output$scatter <- renderPlotly({     #scatter is the name of the plot 
+    # filters the dataset from the widgets
+    plot_data <- immunizations_cleaned %>%
+      filter(County == input$choose_county) %>%
+      filter(K_12_enrollment >= input$enrollment_size[1],
+             K_12_enrollment <= input$enrollment_size[2])
     
- 
     
     # create plot
-    
+    ggplot(plot_data) +
+      geom_point(mapping = aes_string(x = plot_data$K_12_enrollment, 
+                                      y = plot_data$Percent_complete_for_all_immunizations)) +
+      labs(title = "Percent of complete immunizations by School Enrollment size within WA state", 
+           x = "Number of Students (K - 12)",
+           y = "Percent complete for all vacinations")
+  })
   
 }
--- app_ui.R
+++ app_ui.R
@@ -11,25 +11,47 @@ immunizations <- read.csv("immunizations.csv")
 # choose county widget (this widget allows you to 
 # choose which county you want to have the plot focus on)
 
+select_values <- unique(immunizations$County)
+
+choose_county_widget <- selectInput(
+  inputId = "choose_county", 
+  label = "Select a county to observe", 
+  choices = select_values, 
+  selected = "KING")
+
+
 
 
 
 # enrollment size widget (this widget allows you to choose the
 # range of enrollment size)
+immunizations <- immunizations %>%
+  filter(K_12_enrollment != "NA")
 
+enrollment <- c(min(immunizations$K_12_enrollment), 
+                max(immunizations$K_12_enrollment))
 
+enrollment_size_widget <- sliderInput(inputId = "enrollment_size", 
+                                 label = "Choose enrollment size", 
+                                 min = enrollment[1], 
+                                 max = enrollment[2], 
+                                 value = enrollment)
 
 # --------- CREATE PAGES ---------- 
-
+bunnies <- 10 
 page_one <- tabPanel(
   "Page 1",                   #title of the page, what will appear as the tab name
   sidebarLayout(             
     sidebarPanel( 
       # left side of the page 
       # insert widgets or text here -- their variable name(s), NOT the raw code
+      choose_county_widget,
+      enrollment_size_widget
     ),           
     mainPanel(                # typically where you place your plots + texts
       # insert chart and/or text here -- the variable name NOT the code
+      plotlyOutput("scatter"),
+      p("there were", bunnies, " bunnies in the yard")
     )))
 
 
